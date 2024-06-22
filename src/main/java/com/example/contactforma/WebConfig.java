3af49a4094153c325ea00f788b0fed95ef9f2b3f
Lakhveer-singh <67415026+Lakhveer-singh@users.noreply.github.com>
2024-06-22 15:57:48 +0530

    Update WebConfig.java

--- src/main/java/com/example/contactforma/WebConfig.java
+++ src/main/java/com/example/contactforma/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:5173")  // Update the port here
+            .allowedOrigins("http://localhost:5173")  
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(false)
